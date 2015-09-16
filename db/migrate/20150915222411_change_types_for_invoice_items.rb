56a62021782570a114e2e31e0181a2944d8f3fa1
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-16 11:52:38 -0600

    unit prices are big decimals

--- db/migrate/20150915222411_change_types_for_invoice_items.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTypesForInvoiceItems < ActiveRecord::Migration
-  def change
-    remove_column :invoice_items, :quantity, :string
-    remove_column :invoice_items, :unit_price, :string
-  end
-end
--- db/migrate/20150915222609_add_columns_to_invoice_items.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToInvoiceItems < ActiveRecord::Migration
-  def change
-    add_column :invoice_items, :quantity, :integer
-
-    add_column :invoice_items, :unit_price, :integer
-  end
-end
